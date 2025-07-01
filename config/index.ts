7d9c08cf77546c40aa5df3bc08c06be95f6d40b8
tanujgrover9 <tanujgrover07@gmail.com>
2025-07-01 21:47:27 +0530

    first commit

---
+++ config/index.ts
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Konnect",
+  description: "Modern Next.js powered Video calling app",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "getstream",
+    "stream",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "zoom-clone",
+    "realtime-video",
+    "realtime-chat",
+    "live-audio",
+    "live-video",
+    "live-chat",
+    "radix-toast",
+    "lucide-react",
+    "next-themes",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "ts",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Anshuman SIngh",
+    url: "https://github.com/tanujgrover9",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "",
+} as const;
